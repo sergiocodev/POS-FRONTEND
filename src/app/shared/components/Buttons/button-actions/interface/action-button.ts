1c25f121f399921aab57cbd1a85d802312499822
sergiocodev <sergio.sabino@unas.edu.pe>
2026-02-03 08:28:49 -0500

    .

--- src/app/shared/components/Buttons/button-actions/interface/action-button.ts
+++
@@ -1,9 +0,0 @@
-export interface ActionButton {
-    name: string;
-    icon: string;
-    class?: string;
-    backgroundColor?: string;
-    color?: string;
-    label?: string;
-    size?: 'small' | 'medium' | 'large';
-}
